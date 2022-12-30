8eff9691f39e5f4f2e1e4d5f276b4b0841a47790
xarthaa <manager.arthachan@gmail.com>
2022-12-31 02:25:55 +0800

    push server

--- app/Models/Traits/CreateInvoicePdf.php
+++ app/Models/Traits/CreateInvoicePdf.php
@@ -1,27 +1,46 @@
 <?php
 
 namespace App\Models\Traits;
+
 use LaravelDaily\Invoices\Invoice;
 use LaravelDaily\Invoices\Classes\Buyer;
 use LaravelDaily\Invoices\Classes\InvoiceItem;
 
-trait CreateInvoicePdf {
+trait CreateInvoicePdf
+{
     public function createInvoice($order, $orderDetails, $additionalOrders, $orderReq)
     {
         $items = [];
 
-        $customer = new Buyer([
-            'name' => $orderReq->name,
-            'custom_fields' => [
-                'telepon' => $orderReq->phone,
-                'kota' => $orderReq->city,
-                'alamat lengkap' => $orderReq->address,
-                'pembayaran' => $order->channel,
-                'jenis kelamin' => $orderReq->getCustomerSex(),
-                'tanggal' => $orderReq->service_due->format('j F Y H:i'),
-                'catatan tambahan' => $orderReq->notes,
-            ],
-        ]);
+        if ($order->service['slug'] !== 'hair-care') {
+            $customer = new Buyer([
+                'name' => $orderReq->name,
+                'custom_fields' => [
+                    'telepon' => $orderReq->phone,
+                    'kota' => $orderReq->city,
+                    'alamat lengkap' => $orderReq->address,
+                    'pembayaran' => $order->channel,
+                    'jenis kelamin pembeli' => $orderReq->getCustomerSex(),
+                    'jenis kelamin terapis' => $orderReq->getTherapistSex(),
+                    'tanggal' => $orderReq->service_due->format('j F Y H:i'),
+                    'catatan tambahan' => $orderReq->notes,
+                ],
+            ]);
+        } else {
+            $customer = new Buyer([
+                'name' => $orderReq->name,
+                'custom_fields' => [
+                    'telepon' => $orderReq->phone,
+                    'kota' => $orderReq->city,
+                    'alamat lengkap' => $orderReq->address,
+                    'pembayaran' => $order->channel,
+                    'jenis kelamin pembeli' => $orderReq->getCustomerSex(),
+                    'pilihan stylish' => $orderReq->getStyleSex(),
+                    'tanggal' => $orderReq->service_due->format('j F Y H:i'),
+                    'catatan tambahan' => $orderReq->notes,
+                ],
+            ]);
+        }
 
         foreach ($orderDetails as $item) {
             $items[] = (new InvoiceItem())->title($item->service_name)->pricePerUnit($item->price);
@@ -31,7 +50,7 @@ trait CreateInvoicePdf {
             $items[] = (new InvoiceItem())->title($item->service_name)->pricePerUnit($item->price)->quantity($item->quantity);
         }
 
-        if($order->hasDp()) {
+        if ($order->hasDp()) {
             $items[] = (new InvoiceItem())->title("Uang Jaminan")->pricePerUnit(-$order->down_payment)->quantity(1);
         }
 
