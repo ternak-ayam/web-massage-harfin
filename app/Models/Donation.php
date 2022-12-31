e34aece26f2486a39346be1412f62abec5a8246e
xarthaa <manager.arthachan@gmail.com>
2022-12-31 09:53:03 +0800

    bug fix

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\GetPrice;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    use HasFactory;
+    use HasFactory, GetPrice;
 
     protected $fillable = [
         'id',
@@ -20,6 +21,11 @@ class Donation extends Model
     protected $attributes = [
         'status' => self::PENDING
     ];
+
+    public function getFormattedAmount()
+    {
+        return $this->formattedPrice($this->amount);
+    }
 
     const PENDING = "PENDING";
     const SETTLE  = "SETTLE";
